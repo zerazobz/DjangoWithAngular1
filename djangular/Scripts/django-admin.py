5e522da7494d935c289dc95b818bfeb3a47d9120
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-30 17:47:32 -0500

    Creating Django project

---
+++ djangular/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\microdata\documents\eao\wa\src\djangowithangular1\djangowithangular1\djangular\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
