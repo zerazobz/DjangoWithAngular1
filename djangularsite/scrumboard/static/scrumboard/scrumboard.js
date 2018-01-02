282c733122a828218602e8ea4baf86f1556e168a
Erick Asto Oblitas <erickastooblitas@gmail.com>
2018-01-02 12:33:01 -0500

    ->Angular integrated and functional

---
+++ djangularsite/scrumboard/static/scrumboard/scrumboard.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    angular.module('scrumboard.demo', [])
+        .controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
+
+    function ScrumboardController($scope, $http) {
+        $scope.add = function(list, title) {
+            var card = {
+                title: title
+            };
+            list.cards.push(card);
+        };
+
+        $scope.data = [];
+
+        $http.get("/scrumboard/lists/").then(function(response) {
+            $scope.data = response.data;
+        });
+    }
+
+})();
